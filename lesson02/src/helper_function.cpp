e6722fc0f8c0c55aaa4473051a967518c4c3d837
SingularGamesStd <kirgrisha04@mail.ru>
2021-09-15 10:12:29 +0300

    done lesson02

--- lesson02/src/helper_function.cpp
+++ lesson02/src/helper_function.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 
+using namespace std;
 
 void print2DArray(std::vector<std::vector<int>> array2d) {
     for (int j = 0; j < array2d.size(); ++j) { // пробегаем по каждой строке
@@ -10,9 +11,10 @@ void print2DArray(std::vector<std::vector<int>> array2d) {
         // TODO 24 выведите все элементы этой строчки в одну строку в консоли через пробел
         for (int i = 0; i < n; ++i) {
             int value = array2d[j][i]; // взяли j-ую строку, а из нее - i-ый элемент
+            cout << value << " ";
             // выведите в консоль это число
         }
-
+        cout << "\n";
         // TODO 25 не забудьте что надо делать перевод строки после каждого выведенного ряда чисел
     }
 }

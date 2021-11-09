5c09fde2737838ecfc5a099063077dc5748688a1
SingularGamesStudio <40793299+SingularGamesStudio@users.noreply.github.com>
2021-11-10 00:11:30 +0300

    lesson 7 does not work

--- lesson05/src/main.cpp
+++ lesson05/src/main.cpp
@@ -11,32 +11,29 @@ using namespace cv;
 
 string resultsDir = "lesson05/resultsData/";
 
-void testBGRToGray() {
-    std::string name = "valve";
-    cv::Mat img = cv::imread("lesson05/data/" + name + ".jpg");
-    rassert(!img.empty(), 23981920813);
+float pi = acos(-1);
 
-    cv::Mat gray = convertBGRToGray(img);
-
-    cv::imwrite(resultsDir + name + "_grey.jpg", gray);
-}
-
-void testSobel(const std::string &name) {
+void test(const std::string &name) {
     cv::Mat img = cv::imread("lesson05/data/" + name + ".jpg");
     rassert(!img.empty(), 23981920813);
     Mat gray = convertBGRToGray(img);
-    // т.е. посчитайте производную по x и по y (в каждом пикселе хранятся две эти производные)
-    cv::Mat dxy = sobelDXY(gray); // обратите внимание что внутри ждут черно-белую картинку, значит нашу картинку надо перед Собелем преобразовать
-    cout << "a";
-    cv::Mat dx = convertDXYToDX(dxy);
-    cv::imwrite(resultsDir + name + "_dx.jpg", dx);
 
-    cv::Mat dy = convertDXYToDY(dxy);
-    cv::imwrite(resultsDir + name + "_dy.jpg", dy);
+    cv::Mat dxy = sobelDXY(gray);
+    cv::Mat grad = convertDXYToGradientLength(dxy);
+    cv::imwrite(resultsDir + name + "_grad.jpg", grad);
+    Scalar color(0);
+    Mat result(360, sqrt(grad.cols*grad.cols+grad.rows*grad.rows)+10, CV_32FC1);
+    for(int i = 0; i<grad.rows; i++){
+        for(int j = 0; j<grad.cols; j++){
+            for(int f = 0; f<360; f++){
+                float r = i*abs(sin(((float)f)*pi/180.0))+j*abs(cos(((float)f)*pi/180.0));
+                result.at<float>(f, (int) r) += grad.at<float>(i, j)*100;
+                    //cout << r << " " << sqrt(grad.cols*grad.cols+grad.rows*grad.rows)+10;
+            }
+        }
+    }
 
-    cv::Mat gradientStrength = convertDXYToGradientLength(dxy);
-    cv::imwrite(resultsDir + name + "_gradientLength.jpg", gradientStrength);
-    // для valve.jpg должно быть похоже на картинку с википедии - https://ru.wikipedia.org/wiki/%D0%9E%D0%BF%D0%B5%D1%80%D0%B0%D1%82%D0%BE%D1%80_%D0%A1%D0%BE%D0%B1%D0%B5%D0%BB%D1%8F
+    cv::imwrite(resultsDir + name + "_result.jpg", result);
 }
 
 int main() {
@@ -44,21 +41,19 @@ int main() {
         if (!filesystem::exists(resultsDir)) { // если папка еще не создана
             filesystem::create_directory(resultsDir); // то создаем ее
         }
-        testBGRToGray();
-
         for (int i = 1; i <= 4; ++i) {
-            testSobel("line0" + std::to_string(i));
+            test("line0" + std::to_string(i));
         }
 
         for (int i = 1; i <= 4; ++i) {
-            testSobel("line1" + std::to_string(i));
+            test("line1" + std::to_string(i));
         }
 
-        testSobel("line21_water_horizont");
-        testSobel("multiline1_paper_on_table");
-        testSobel("multiline2_paper_on_table");
+        test("line21_water_horizont");
+        test("multiline1_paper_on_table");
+        test("multiline2_paper_on_table");
 
-        testSobel("valve");
+        test("valve");
 
         return 0;
     } catch (const std::exception &e) {

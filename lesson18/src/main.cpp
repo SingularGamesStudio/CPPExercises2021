bf4560cd87e1bac792edd18caa55f305489e743a
SingularGamesStd <kirgrisha04@mail.ru>
2022-04-06 10:55:29 +0300

    lesson19 started

--- lesson18/src/main.cpp
+++ lesson18/src/main.cpp
@@ -9,21 +9,25 @@
 #include <iostream>
 #include <filesystem>
 #include <memory>
+#include <random>
 
 #include <libutils/rasserts.h>
 #include <libutils/fast_random.h>
 
+using namespace std;
+using namespace cv;
 
 // Эта функция говорит нам правда ли пиксель отмаскирован, т.е. отмечен как "удаленный", т.е. белый
 bool isPixelMasked(cv::Mat mask, int j, int i) {
     rassert(j >= 0 && j < mask.rows, 372489347280017);
     rassert(i >= 0 && i < mask.cols, 372489347280018);
     rassert(mask.type() == CV_8UC3, 2348732984792380019);
-
-    // TODO проверьте белый ли пиксель
-    return false;
+    Vec3b color = mask.at<Vec3b>(i, j);
+    if(color[0]+color[1]+color[2]>100)
+        return 1;
+    return 0;
 }
-
+mt19937 rnd(42);
 void run(int caseNumber, std::string caseName) {
     std::cout << "_________Case #" << caseNumber << ": " <<  caseName << "_________" << std::endl;
 
@@ -32,9 +36,8 @@ void run(int caseNumber, std::string caseName) {
     rassert(!original.empty(), 324789374290018);
     rassert(!mask.empty(), 378957298420019);
 
-    // TODO напишите rassert сверяющий разрешение картинки и маски
-    // TODO выведите в консоль это разрешение картинки
-    // std::cout << "Image resolution: " << ... << std::endl;
+    rassert(mask.cols==original.cols && mask.rows==original.rows, 1283912);
+     std::cout << "Image resolution: " << mask.cols << "x" << mask.rows << std::endl;
 
     // создаем папку в которую будем сохранять результаты - lesson18/resultsData/ИМЯ_НАБОРА/
     std::string resultsDir = "lesson18/resultsData/";
@@ -45,64 +48,64 @@ void run(int caseNumber, std::string caseName) {
     if (!std::filesystem::exists(resultsDir)) { // если папка еще не создана
         std::filesystem::create_directory(resultsDir); // то создаем ее
     }
+    vector<pair<Mat, Mat>> pyramid;
+    Mat img = original.clone();
+    const int PYRAMID_MIN_SIZE = 20; // до какой поры уменьшать картинку? давайте уменьшать пока картинка больше 20 пикселей
+    while (img.rows > PYRAMID_MIN_SIZE && img.rows > PYRAMID_MIN_SIZE) { // или пока больше (2 * размер окна для оценки качества)
+        pyramid.push_back({img.clone(), mask.clone()}); // мы могли бы воспользоваться push_back но мы хотим вставлять картинки в начало вектора
+        cv::pyrDown(img, img); // эта функция уменьшает картинку в два раза
+        cv::pyrDown(mask, mask); // эта функция уменьшает картинку в два раза
+    }
+    cv::Mat shiftslast;
+    reverse(pyramid.begin(), pyramid.end());
+    for(int layer = 0; layer<pyramid.size(); layer++){
+        img = pyramid[layer].first;
+        mask = pyramid[layer].second;
+        string nowdir = resultsDir+"/layer_"+ to_string(layer)+"/";
+        if (!std::filesystem::exists(nowdir)) { // если папка еще не создана
+            std::filesystem::create_directory(nowdir); // то создаем ее
+        }
+        // сохраняем в папку с результатами оригинальную картинку и маску
+        cv::imwrite(nowdir + "0original.png", original);
+        cv::imwrite(nowdir + "1mask.png", mask);
 
-    // сохраняем в папку с результатами оригинальную картинку и маску
-    cv::imwrite(resultsDir + "0original.png", original);
-    cv::imwrite(resultsDir + "1mask.png", mask);
+        int cntmasked = 0;
+        for(int i = 0; i<mask.rows; i++){
+            for(int j = 0; j<mask.cols; j++){
+                if(isPixelMasked(mask, i, j)){
+                    img.at<Vec3b>(i, j) = Vec3b(rnd()%255, rnd()%255, rnd()%255);
+                    cntmasked++;
+                }
+            }
+        }
+        cout << "Number of masked pixels: " <<  cntmasked << "/" << mask.rows*mask.cols << " = " << cntmasked*100.0/mask.rows/mask.cols << "%";
 
-    // TODO замените белым цветом все пиксели в оригинальной картинке которые покрыты маской
-    // TODO сохраните в папку с результатами то что получилось под названием "2_original_cleaned.png"
-    // TODO посчитайте и выведите число отмаскированных пикселей (числом и в процентах) - в таком формате:
-    // Number of masked pixels: 7899/544850 = 1%
+        cv::Mat shifts(img.rows, img.cols, CV_32SC2, cv::Scalar(0, 0)); // матрица хранящая смещения, изначально заполнена парами нулей
+        if(layer!=0){
+            for(int i = 0; i<mask.rows; i++){
+                for(int j = 0; j<mask.cols; j++){
+                    shifts.at<Vec2i>(i, j) = Vec2i(shiftslast.at<Vec2i>(i/2, j/2)[0]*2, shiftslast.at<Vec2i>(i/2, j/2)[1]*2);
+                }
+            }
+        }
+        for(int i = 0; i<mask.rows; i++){
+            for(int j = 0; j<mask.cols; j++){
 
-    FastRandom random(32542341); // этот объект поможет вам генерировать случайные гипотезы
-
-    // TODO 10 создайте картинку хранящую относительные смещения - откуда брать донора для заплатки, см. подсказки про то как с нею работать на сайте
-    // TODO 11 во всех отмаскированных пикселях: заполните эту картинку с относительными смещениями - случайными смещениями (но чтобы они и их окрестность 5х5 не выходила за пределы картинки)
-    // TODO 12 во всех отмаскированных пикселях: замените цвет пиксела А на цвет пикселя Б на который указывает относительное смещение пикселя А
-    // TODO 13 сохраните получившуюся картинку на диск
-    // TODO 14 выполняйте эти шаги 11-13 много раз, например 1000 раз (оберните просто в цикл, сохраняйте картинку на диск только на каждой десятой или сотой итерации)
-    // TODO 15 теперь давайте заменять значение относительного смещения на новой только если новая случайная гипотеза - лучше старой, добавьте оценку "насколько смещенный патч 5х5 похож на патч вокруг пикселя если их наложить"
-    //
-    // Ориентировочный псевдокод-подсказка получившегося алгоритма:
-    // cv::Mat shifts(...); // матрица хранящая смещения, изначально заполнена парами нулей
-    // cv::Mat image = original; // текущая картинка
-    // for (100 раз) {
-    //     for (пробегаем по всем пикселям j,i) {
-    //         if (если этот пиксель не отмаскирован)
-    //             continue; // пропускаем т.к. его менять не надо
-    //         cv::Vec2i dxy = смотрим какое сейчас смещение для этого пикселя в матрице смещения
-    //         int (nx, ny) = (i + dxy.x, j + dxy.y); // ЭТО НЕ КОРРЕКТНЫЙ КОД, но он иллюстрирует как рассчитать координаты пикселя-донора из которого мы хотим брать цвет
-    //         currentQuality = estimateQuality(image, j, i, ny, nx, 5, 5); // эта функция (создайте ее) считает насколько похож квадрат 5х5 приложенный центром к (i, j)
-    //                                                                                                                        на квадрат 5х5 приложенный центром к (nx, ny)
-    //
-    //         int (rx, ry) = random.... // создаем случайное смещение относительно нашего пикселя, воспользуйтесь функцией random.next(...);
-    //                                      (окрестность вокруг пикселя на который укажет смещение - не должна выходить за пределы картинки и не должна быть отмаскирована)
-    //         randomQuality = estimateQuality(image, j, i, j+ry, i+rx, 5, 5); // оцениваем насколько похоже будет если мы приложим эту случайную гипотезу которую только что выбрали
-    //
-    //         if (если новое качество случайной угадайки оказалось лучше старого) {
-    //             то сохраняем (rx,ry) в картинку смещений
-    //             и в текущем пикселе кладем цвет из пикселя на которого только что смотрели (цент окрестности по смещению)
-    //             (т.е. мы не весь патч сюда кладем, а только его центральный пиксель)
-    //         } else {
-    //             а что делать если новая случайная гипотеза хуже чем то что у нас уже есть?
-    //         }
-    //     }
-    //     не забываем сохранить на диск текущую картинку
-    //     а как численно оценить насколько уже хорошую картинку мы смогли построить? выведите в консоль это число
-    // }
+            }
+        }
+        shiftslast = shifts.clone();
+    }
 }
 
 
 int main() {
     try {
         run(1, "mic");
-        // TODO протестируйте остальные случаи:
 //        run(2, "flowers");
 //        run(3, "baloons");
 //        run(4, "brickwall");
 //        run(5, "old_photo");
-//        run(6, "your_data"); // TODO придумайте свой случай для тестирования (рекомендуется не очень большое разрешение, например 300х300)
+//        run(6, "your_data");
 
         return 0;
     } catch (const std::exception &e) {
